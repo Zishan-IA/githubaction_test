6140dfb59f2b9786fd25860fb14da44cbef3768f
zishan sayyed <123532227+Zishan-IA@users.noreply.github.com>
2023-09-17 15:39:12 +0530

    Update setp1-GA4_overalltraffic.R

--- setp1-GA4_overalltraffic.R
+++ setp1-GA4_overalltraffic.R
@@ -41,6 +41,12 @@ ga_result <- GA_traffic(ga4_propertyId, startDate, endDate)
 
 #final_ga_data %>%write_rds("final_ga_data.rds")
 
+library(googledrive)
+# Either specify the user:
+drive_auth(email = "zishan.sayyed@interactiveavenues.com")
+# Or, if you only use one Google identity, you can be more vague:
+drive_auth(email = TRUE)
+
 Sheet_Name <- "ga_traffic"
 gs_upload(email_id, sheet_id, Sheet_Name, ga_result)
 
